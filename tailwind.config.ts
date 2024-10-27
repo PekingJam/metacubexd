60a5aeddbf1a1d09009103c5752047998ce78266
orange <a@b.c>
2024-10-27 23:59:25 +0800

    feat: add initl demo hello

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,20 @@ import { Config } from 'tailwindcss'
 export default {
   content: ['src/**/*.{css,ts,tsx}'],
   plugins: [daisyui, corvu],
-  daisyui: { themes: true },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          accent: '#ffffff',
+          success: '#1aad19',
+          info: '#0000ff',
+          warning: '#f8d007',
+          error: '#ff0000',
+        },
+      },
+      'dark',
+    ],
+  },
   theme: {
     extend: {
       fontFamily: {
