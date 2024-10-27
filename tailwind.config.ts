7d8c388c3bab92998374bc0594fdc435856c5376
orange <a@b.c>
2024-10-28 00:28:16 +0800

    feat: add initl demo hello

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,31 @@
 import corvu from '@corvu/tailwind'
 import daisyui from 'daisyui'
-import { light } from 'daisyui/src/theming/themes'
 import { Config } from 'tailwindcss'
 
 export default {
   content: ['src/**/*.{css,ts,tsx}'],
   plugins: [daisyui, corvu],
   daisyui: {
-    themes: [
-      {
-        light: {
-          ...light,
-          accent: '#ffffff',
-          success: '#1aad19',
-          info: '#1E90FF',
-          warning: '#f8d007',
-          error: '#ff0000',
-        },
-      },
-      'dark',
-    ],
+    themes: ['light', 'dark'],
+    styled: true,
+    base: true,
+    utils: true,
+    // 这里自定义 light 主题
+    // 通过 extending 来修改 existing themes
+    darkTheme: 'dark',
   },
   theme: {
     extend: {
       fontFamily: {
         twemoji: ['system-ui', 'Twemoji Mozilla', 'Fira Sans', 'monospace'],
         'no-twemoji': ['system-ui', 'Fira Sans', 'monospace'],
+      },
+      colors: {
+        accent: '#ffffff',
+        success: '#1aad19',
+        info: '#1E90FF',
+        warning: '#f8d007',
+        error: '#ff0000',
       },
     },
   },
