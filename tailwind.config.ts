1d122ef970462976256318ab4a6d2945b738d55b
orange <a@b.c>
2024-10-28 01:16:06 +0800

    feat: 删除大量无用的主题，修改默认Liht主题，使其更美观

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,31 @@
 import corvu from '@corvu/tailwind'
 import daisyui from 'daisyui'
+import themes from 'daisyui/src/theming/themes'
 import { Config } from 'tailwindcss'
 
 export default {
   content: ['src/**/*.{css,ts,tsx}'],
   plugins: [daisyui, corvu],
   daisyui: {
-    themes: ['light', 'dark'],
-    styled: true,
-    base: true,
-    utils: true,
-    // 这里自定义 light 主题
-    // 通过 extending 来修改 existing themes
-    darkTheme: 'dark',
+    themes: [
+      {
+        light: {
+          ...themes['light'],
+          neutral: '#ffffff',
+          success: '#1aad19',
+          info: '#1E90FF',
+          warning: '#f8d007',
+          error: '#ff0000',
+        },
+      },
+      'dark',
+    ],
   },
   theme: {
     extend: {
       fontFamily: {
         twemoji: ['system-ui', 'Twemoji Mozilla', 'Fira Sans', 'monospace'],
         'no-twemoji': ['system-ui', 'Fira Sans', 'monospace'],
-      },
-      colors: {
-        neutral: '#ffffff',
-        success: '#1aad19',
-        info: '#1E90FF',
-        warning: '#f8d007',
-        error: '#ff0000',
       },
     },
   },
